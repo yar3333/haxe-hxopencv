ec79ae109d4e6273c4cf3634654eee230e5a12b0
Yaroslav <Yaroslav@localhost>
2013-07-03 20:06:53 +0400

    + workable test

--- tests/neko/make.cmd
+++ tests/neko/make.cmd
@@ -1,4 +1,2 @@
-set NEKOPATH=%NEKOPATH%;..\..\library\ndll\Windows
-
 haxe -cp ../../library -cp . -main Main -neko Main.n
-neko Main.n
+call run.cmd
---
+++ tests/neko/run.cmd
@@ -0,0 +1,3 @@
+cd %~dp0
+set NEKOPATH=%NEKOPATH%;..\..\library\ndll\Windows
+neko Main.n
