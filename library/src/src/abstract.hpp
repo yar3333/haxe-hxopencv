8d689bdeac60a2819af44048793a7e60af6544d9
Yaroslav <Yaroslav@localhost>
2013-07-07 21:21:17 +0400

    * minor

--- library/src/src/abstract.hpp
+++ library/src/src/abstract.hpp
@@ -20,41 +20,12 @@ using namespace cv;
 
 ////////////////////////////////////////////////////////////////////
 
-void deleteObject(value obj);
+extern void deleteObject(value obj);
 
 ////////////////////////////////////////////////////////////////////
 
-/*inline value iplImageToValue(IplImage *image)
-{
-	value r = alloc_abstract(k_IplImage, image);
-	val_gc(r, neko_cvReleaseImage);
-	return r;
-}
-
-inline IplImage *valueToIplImage(value image)
-{
-	//val_check_kind(image, k_IplImage);
-	return (IplImage *)val_data(image);
-}*/
-
-inline value matToValue(Mat &mat)
-{
-	value r = alloc_abstract(k_Mat, new Mat(mat));
-	val_gc(r, deleteObject);
-	return r;
-}
-
-inline Mat &valueToMat(value mat)
-{
-	if (!val_is_abstract(mat) || !val_is_kind(mat,k_Mat)) failure("Mat type expected.");
-	return *((Mat*)val_data(mat));
-}
-
-
-inline value pointToValue(Point p)
-{
-	value r = alloc_object(NULL);
-	alloc_field(r, val_id("x"), alloc_int(p.x));
-	alloc_field(r, val_id("y"), alloc_int(p.y));
-	return r;
-}
+extern value iplImageToValue(IplImage *image);
+extern IplImage *valueToIplImage(value image);
+extern value matToValue(Mat mat);
+extern Mat &valueToMat(value mat);
+extern value pointToValue(Point p);
